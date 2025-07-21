d421d3138f2d660a7f020456640de1d05f9725d0
LucianoBDN <luciano.bordon438@gmail.com>
2025-07-21 17:53:40 -0300

    controladores y rutas de usuario agregadas y funcionando

---
+++ backend/controllers/usersControllers.js
@@ -0,0 +1,84 @@
+const User = require("../models/users");
+const bcrypt = require("bcryptjs");
+
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error });
+  }
+};
+
+const getUserById = async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error });
+  }
+};
+
+const createUser = async (req, res) => {
+  try {
+    const role = req.body.role || "admin"; // Default to "admin" if not provided
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const newUser = await User.create({
+      user_name: req.body.user_name,
+      role: role,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    res.status(201).json(newUser);
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res.status(500).json({ message: "Internal server error", error });
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const role = req.body.role || "admin";
+
+    const updatedFields = {
+      user_name: req.body.user_name,
+      role: role,
+      email: req.body.email,
+    };
+    if (req.body.password) {
+      updatedFields.password = await bcrypt.hash(req.body.password, 10);
+    }
+    await user.update(updatedFields);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error });
+  }
+};
+
+const deleteUser = async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    await user.destroy();
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error });
+  }
+};
+
+module.exports = {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+};
--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,16 @@ db = require("./models/relaciones.js")
 
 const express = require('express')
 const app = express()
-
 app.use(cors())
+app.use(express.json())
+
+// Api routes
+const usersRoutes = require("./routes/usersRouter.js");
+
+app.use("/users", usersRoutes);
+
+
+
 
 const port =  3030;
 app.listen(port,async ()=>{
