2f25442e8874841204917462904954e9275cc26d
LucianoBDN <luciano.bordon438@gmail.com>
2025-07-22 22:10:55 -0300

    Creacion de rutas posts

--- backend/index.js
+++ backend/index.js
@@ -10,13 +10,16 @@ app.use(express.json())
 
 // Api routes
 const usersRoutes = require("./routes/usersRouter.js");
+const postsRoutes = require("./routes/postsRouter.js");
 
 app.use("/users", usersRoutes);
+app.use("/posts", postsRoutes);
 
 
 
 
-const port =  3030;
-app.listen(port,async ()=>{
+const PORT =  3030;
+app.listen(PORT,async ()=>{
+    console.log(`Servidor corriendo en http://localhost:${PORT}`)
     await conexionBaseDeDatos();
 })
